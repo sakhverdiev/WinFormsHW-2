60e556b1956a16bc6400f60da4172151c9d4f91d
Sanan Akhverdiev <sakhverdiev@gmail.com>
2023-10-11 14:36:01 +0400

    ..

---
+++ QuestionnaireWF/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace QuestionnaireWF
+{
+    partial class Anket
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Anket));
+            this.groupBox_Anket = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dateTimePickerBirthdate = new System.Windows.Forms.DateTimePicker();
+            this.maskedTextBoxPhone = new System.Windows.Forms.MaskedTextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.groupBox_Anket.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox_Anket
+            // 
+            this.groupBox_Anket.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox_Anket.Controls.Add(this.button1);
+            this.groupBox_Anket.Controls.Add(this.btnAdd);
+            this.groupBox_Anket.Controls.Add(this.dateTimePickerBirthdate);
+            this.groupBox_Anket.Controls.Add(this.maskedTextBoxPhone);
+            this.groupBox_Anket.Controls.Add(this.textBoxEmail);
+            this.groupBox_Anket.Controls.Add(this.textBoxSurname);
+            this.groupBox_Anket.Controls.Add(this.textBoxName);
+            this.groupBox_Anket.Controls.Add(this.label5);
+            this.groupBox_Anket.Controls.Add(this.label4);
+            this.groupBox_Anket.Controls.Add(this.label3);
+            this.groupBox_Anket.Controls.Add(this.label2);
+            this.groupBox_Anket.Controls.Add(this.lblName);
+            this.groupBox_Anket.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox_Anket.Location = new System.Drawing.Point(180, 69);
+            this.groupBox_Anket.Name = "groupBox_Anket";
+            this.groupBox_Anket.Size = new System.Drawing.Size(600, 419);
+            this.groupBox_Anket.TabIndex = 7;
+            this.groupBox_Anket.TabStop = false;
+            this.groupBox_Anket.Text = "Anket";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(428, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 46);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(276, 353);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(122, 46);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // dateTimePickerBirthdate
+            // 
+            this.dateTimePickerBirthdate.CalendarFont = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerBirthdate.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerBirthdate.Location = new System.Drawing.Point(128, 280);
+            this.dateTimePickerBirthdate.Name = "dateTimePickerBirthdate";
+            this.dateTimePickerBirthdate.Size = new System.Drawing.Size(424, 36);
+            this.dateTimePickerBirthdate.TabIndex = 11;
+            this.dateTimePickerBirthdate.Value = new System.DateTime(2021, 8, 3, 0, 0, 0, 0);
+            this.dateTimePickerBirthdate.ValueChanged += new System.EventHandler(this.DateTimePickerBirthdate_ValueChanged);
+            // 
+            // maskedTextBoxPhone
+            // 
+            this.maskedTextBoxPhone.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBoxPhone.Location = new System.Drawing.Point(128, 222);
+            this.maskedTextBoxPhone.Mask = "(000) 000-00-00";
+            this.maskedTextBoxPhone.Name = "maskedTextBoxPhone";
+            this.maskedTextBoxPhone.Size = new System.Drawing.Size(424, 36);
+            this.maskedTextBoxPhone.TabIndex = 10;
+            this.maskedTextBoxPhone.TextChanged += new System.EventHandler(this.MaskedTextBoxPhone_TextChanged);
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEmail.Location = new System.Drawing.Point(128, 164);
+            this.textBoxEmail.MaxLength = 30;
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(424, 36);
+            this.textBoxEmail.TabIndex = 8;
+            this.textBoxEmail.TextChanged += new System.EventHandler(this.TextBoxEmail_TextChanged);
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSurname.Location = new System.Drawing.Point(128, 112);
+            this.textBoxSurname.MaxLength = 30;
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(424, 36);
+            this.textBoxSurname.TabIndex = 7;
+            this.textBoxSurname.TextChanged += new System.EventHandler(this.TextBoxSurname_TextChanged);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxName.Location = new System.Drawing.Point(128, 62);
+            this.textBoxName.MaxLength = 30;
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(424, 36);
+            this.textBoxName.TabIndex = 6;
+            this.textBoxName.TextChanged += new System.EventHandler(this.TextBoxName_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 24);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Birthdate";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Tel";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Surname";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(6, 72);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(58, 24);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name";
+            // 
+            // Anket
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::QuestionnaireWF.Properties.Resources.download;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(986, 561);
+            this.Controls.Add(this.groupBox_Anket);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MinimumSize = new System.Drawing.Size(1002, 600);
+            this.Name = "Anket";
+            this.Text = "Form1";
+            this.groupBox_Anket.ResumeLayout(false);
+            this.groupBox_Anket.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox_Anket;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBirthdate;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxPhone;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ QuestionnaireWF/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuestionnaireWF
+{
+    public partial class Anket : Form
+    {
+        Person user = new Person();
+
+        public Anket()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBoxName_TextChanged(object sender, EventArgs e)
+        {
+            user.Name = textBoxName.Text;
+        }
+
+        private void TextBoxSurname_TextChanged(object sender, EventArgs e)
+        {
+            user.Surname = textBoxSurname.Text;
+        }
+
+        private void TextBoxEmail_TextChanged(object sender, EventArgs e)
+        {
+            user.Email = textBoxEmail.Text;
+        }
+
+
+        private void DateTimePickerBirthdate_ValueChanged(object sender, EventArgs e)
+        {
+            user.BirthDate = dateTimePickerBirthdate.Value;
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            btnAdd.Text = "Add";
+            if (!string.IsNullOrWhiteSpace(textBoxName.Text))
+            {
+                if (groupBox_Anket.Controls.OfType<TextBox>().Any(tb => string.IsNullOrEmpty(tb.Text))
+                    || !maskedTextBoxPhone.MaskCompleted)
+                {
+                    MessageBox.Show("Fill in the blanks!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else
+                {
+                    var options = new JsonSerializerOptions();
+                    options.WriteIndented = true;
+                    options.Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+                    var textJson = JsonSerializer.Serialize(user, options);
+                    File.WriteAllText(textBoxName.Text + ".json", textJson);
+                    MessageBox.Show("The questionnaire was completed successfully!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    groupBox_Anket.Controls.OfType<TextBox>().ToList().ForEach(tb => tb.Text = string.Empty);
+                    maskedTextBoxPhone.Text = string.Empty;
+                    dateTimePickerBirthdate.Value = DateTime.Now;
+                }
+            }
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            groupBox_Anket.Controls.OfType<TextBox>().ToList().ForEach(tb => tb.Text = string.Empty);
+            maskedTextBoxPhone.Text = string.Empty;
+            dateTimePickerBirthdate.Value = DateTime.Now;
+        }
+
+        private void MaskedTextBoxPhone_TextChanged(object sender, EventArgs e) => user.Phone = maskedTextBoxPhone.Text;
+
+    }
+}
